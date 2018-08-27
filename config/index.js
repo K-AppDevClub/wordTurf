c09a4f550c90744a95c3270854e9c04455561b15
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2018-08-27 21:01:16 +0900

    Change index.js

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../docs/index.html'),
-    assetsRoot: path.resolve(__dirname, '../cordova/docs'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
     productionSourceMap: false,
